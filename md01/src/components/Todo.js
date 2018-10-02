cd7d6dde76b7f564d456b1afb4fda3cb8190eee4
luksinocenci0 <luksinocencio@gmail.com>
2018-10-02 13:32:32 -0300

    -

---
+++ md01/src/components/Todo.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { View, Text } from 'react-native'
+
+const Todo = ({ title }) => (
+  <View>
+    <Text>{title}</Text>
+  </View>
+)
+
+Todo.defaultProps = {
+  title: 'Todo padrão',
+}
+
+Todo.propTypes = {
+  title: PropTypes.string.isRequired,
+}
+
+export default Todo
