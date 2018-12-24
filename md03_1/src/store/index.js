972182a2fb60f7755d382be2e6421a11818eef88
devmeist3r <devmeister@gmail.com>
2018-12-24 15:24:15 -0200

    -

--- md03_1/src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from 'redux'
-
-const middleware = []
-
-const createAppropriateStore = __DEV__ ? console.tron.createStore : createStore
-const store = createAppropriateStore(() => {})
-
-export default store
